490bd8d6f36441b6b1c16d88406734bd7d76f514
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-05-11 10:31:41 +0530

    its done now

--- authentication_firebas/src/app/components/logout/page.js
+++ authentication_firebas/src/app/components/logout/page.js
@@ -2,24 +2,32 @@
 import Link from "next/link";
 import React from "react";
 import { useAuth } from "../../../../library/context";
+import { useRouter } from "next/navigation";
 
 const Logout = () => {
   const { user, handelLogout } = useAuth();
+  const router = useRouter(); 
+
+  const handleLogoutClick = async () => {
+    await handelLogout();         
+    router.push('/components/login');        
+  };
+
   return (
     <div>
       {!user && (
-        <Link href={"components/login"}>
-          <button className="bg-purple-500 p-1 px-5 rounded-lg border-none hover:bg-purple-700 transition hover:cursor-pointer ">
+          <button  onClick={() => router.push('/components/login')} className="bg-purple-500 p-1 px-5 rounded-lg border-none hover:bg-purple-700 transition hover:cursor-pointer ">
             Login
           </button>
-        </Link>
+
       )}
       {user && (
-        <Link href={"/login"}>
-          <button onClick={handelLogout} className="bg-purple-500 p-1 px-5 rounded-lg border-none hover:bg-purple-700 transition hover:cursor-pointer ">
-            Logout
-          </button>
-        </Link>
+        <button 
+          onClick={handleLogoutClick} 
+          className="bg-purple-500 p-1 px-5 rounded-lg border-none hover:bg-purple-700 transition hover:cursor-pointer "
+        >
+          Logout
+        </button>
       )}
     </div>
   );
